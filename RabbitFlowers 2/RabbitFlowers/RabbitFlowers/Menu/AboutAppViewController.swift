07a41dbde6ec59f526f551a2a528702550e1e230
Ivan Nezgovorov <87833835+IvanNez@users.noreply.github.com>
2024-05-24 15:56:24 +0500

    added logic with sent mail, and rate profile

--- RabbitFlowers 2/RabbitFlowers/RabbitFlowers/Menu/AboutAppViewController.swift
+++ RabbitFlowers 2/RabbitFlowers/RabbitFlowers/Menu/AboutAppViewController.swift
@@ -6,11 +6,24 @@
 //
 
 import UIKit
+import StoreKit
 
 class AboutAppViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    @IBAction func writeSupportButton(_ sender: Any) {
+        sentToMail(to: "headshot.kill96@icloud.com")
+    }
+    
+    
+    @IBAction func rateProfile(_ sender: Any) {
+        if let windowScene = view.window?.windowScene {
+                    SKStoreReviewController.requestReview(in: windowScene)
+        }
     }
 }
+
